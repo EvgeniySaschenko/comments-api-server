703873362cdcbe1413163f7016cbe5256d2368a0
Evgeniy Saschenko <evgeniy.saschenko@gmail.com>
2021-09-24 17:00:48 +0300

    fix package.json

--- env.config.js
+++ env.config.js
@@ -6,9 +6,9 @@ let config = {
     secret: "dafafqek54mk45k45kl"
   },
   production: {
-    port: 4444,
-    baseUrlImg: 'http://localhost:4444/images',
-    baseUrl: 'http://localhost:4444/',
+    port: 8888,
+    baseUrlImg: 'https://vue-comments-server.herokuapp.com:8888/images',
+    baseUrl: 'https://vue-comments-server.herokuapp.com:8888/',
     secret: "dafafqek54mk45k45kl"
   },
 }
