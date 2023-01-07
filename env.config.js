1d8feb2293eabd68bab837d9d40b9021ace881fe
Evgeniy <evgeniy@example.com>
2023-01-07 14:41:41 +0200

    port

--- env.config.js
+++ env.config.js
@@ -2,13 +2,12 @@ const mode = process.env.NODE_ENV || "development";
 
 let config = {
   "development": {
-    port: 8888,
     baseUrlImg: 'http://localhost:8888/images',
     baseUrl: 'http://localhost:8888/',
     secret: "dafafqek54mk45k45kl"
   },
   "production": {
-    port: 80,
+    port: null,
     baseUrlImg: '/images',
     baseUrl: '/',
     secret: "dafafqek54mk45k45kl"
